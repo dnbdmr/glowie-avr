d77f236a4385c5655ba51ee0d2991182059d4142
dnbdmr <dnbdmr@users.noreply.github.com>
2019-09-16 19:15:30 -0700

    First Test, not working

--- software/main.c
+++ software/main.c
@@ -1,15 +1,19 @@
-#define F_CPU 4000000
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/power.h>
 #include <avr/pgmspace.h>
+#include <avr/interrupt.h>
 #include <stdlib.h>
 #include "ws2812_config.h"
 #include "light_ws2812.h"
 
+struct cRGB led[1];
+
 void init(void) {
-	clock_prescale_set(clock_div_2);
+	clock_prescale_set(clock_div_1);
+	DDRB |= _BV(PB1);
+
+	DDRB |= _BV(PB0);
 }
 
 void variable_delay_ms(uint16_t i) {
@@ -20,5 +24,18 @@ void variable_delay_ms(uint16_t i) {
 int main(void) {
 	init();
 	while(1) {
+		PORTB |= _BV(PB1);
+		led[0].r=255;led[0].g=0;led[0].b=0;    // Write red to array
+		ws2812_setleds(led,1);
+		_delay_ms(1000);                         // wait for 500ms.
+
+		PORTB &= ~(_BV(PB1));
+		led[0].r=0;led[0].g=255;led[0].b=0;			// green
+		ws2812_setleds(led,1);
+		_delay_ms(1000);
+
+		led[0].r=0;led[0].g=00;led[0].b=255;		// blue
+		ws2812_setleds(led,1);
+		_delay_ms(1000);
 	}
 }
