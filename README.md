058f327ccdc50933fbd7f00d6543623c5d604c3d
dnbdmr <dnbdmr@users.noreply.github.com>
2019-09-18 18:54:19 -0700

    First working commit, need to work on timings, etc...

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 Random fading light
 
+__TODO__
+- Add capacitor to WS2812 board
+- gamma correction?
 
 # Tue 17 Sep 2019 07:01:32 PM PDT
 - starting with digispark running at 16MHz from internal PLL
+# Wed 18 Sep 2019 06:51:35 PM PDT
+- broke usb connector on digispark using usbtinyISP
--- software/main.c
+++ software/main.c
@@ -16,26 +16,71 @@ void init(void) {
 	DDRB |= _BV(PB0);
 }
 
-void variable_delay_ms(uint16_t i) {
+void variable_delay_ms(uint32 i) {
 	while (i--)
 		_delay_ms(1);
 }
 
 int main(void) {
 	init();
+
+	struct cRGB  templed[1];
+	led[0].r = 0xFF;
+	led[0].b = 0x0;
+	led[0].g = 0xFF;
+	ws2812_setleds(led, 1);
+
+	uint8_t ledreg = 0; // 0b00000RGB
+
 	while(1) {
 		PORTB |= _BV(PB1);
-		led[0].r=10;led[0].g=0;led[0].b=0;    // Write red to array
-		ws2812_setleds(led,1);
-		_delay_ms(500);                         // wait for 500ms.
+
+		templed[0].r = rand() & 0xFF;
+		templed[0].g = rand() & 0xFF;
+		templed[0].b = rand() & 0xFF;
+
+		led[0].r = led[0].g = led[0].b = 0;
+		ws2812_setleds(led, 1);
+		_delay_ms(10);
+
+		while (ledreg < 7) { 			//count up
+			if (led[0].r < templed[0].r)
+				led[0].r++;
+			else
+				ledreg |= (1<<2);
+			if (led[0].g < templed[0].g)
+				led[0].g++;
+			else 
+				ledreg |= (1<<1);
+			if (led[0].b < templed[0].b)
+				led[0].b++;
+			else
+				ledreg |= (1<<0);
+			ws2812_setleds(led, 1);
+			_delay_ms(10);
+		}
+
+		variable_delay_ms(rand() & 0xF);
 
 		PORTB &= ~(_BV(PB1));
-		led[0].r=0;led[0].g=10;led[0].b=0;			// green
-		ws2812_setleds(led,1);
-		_delay_ms(500);
 
-		led[0].r=0;led[0].g=00;led[0].b=10;		// blue
-		ws2812_setleds(led,1);
-		_delay_ms(500);
+		while (ledreg > 0) { 			//count down
+			if (led[0].r > 0)
+				led[0].r--;
+			else
+				ledreg &= ~(1<<2);
+			if (led[0].g > 0)
+				led[0].g--;
+			else 
+				ledreg &= ~(1<<1);
+			if (led[0].b > 0)
+				led[0].b--;
+			else
+				ledreg &= ~(1<<0);
+			ws2812_setleds(led, 1);
+			_delay_ms(10);
+		}
+
+		variable_delay_ms(rand() & 0xFFF);
 	}
 }
